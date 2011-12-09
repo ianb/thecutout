a6b1a6dcfc9720c8e180a84de1ba4a8c6651eaaf
Ian Bicking <ianb@colorstudy.com>
2011-12-08 21:40:55 -0600

    initial layout

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = '0.0'
+
+setup(name='logdb',
+      version=version,
+      description="Database that is really just a log",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+      ],
+      )
