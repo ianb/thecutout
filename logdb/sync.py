235c1b4371fe4f3874c118f82ea83955f4104769
Ian Bicking <ianb@colorstudy.com>
2012-07-20 21:43:48 -0500

    Remove auth.  Add syncclient.js serving.  Catch some request errors.

--- logdb/sync.py
+++ logdb/sync.py
@@ -2,31 +2,18 @@ import os
 import shutil
 import time
 import urllib
-import simplejson as json
-import hmac
-import hashlib
+try:
+    import simplejson as json
+except ImportError:
+    import json
 from webob.dec import wsgify
 from webob import Response
 from webob import exc
 from logdb import Database, ExpectationFailed
 
-
-_secret = None
-
-
-def get_secret():
-    global _secret
-    if _secret is not None:
-        return _secret
-    fn = '/tmp/sync-secret.txt'
-    if os.path.exists(fn):
-        with open(fn, 'rb') as fp:
-            _secret = fp.read().strip()
-    else:
-        with open(fn, 'wb') as fp:
-            _secret = os.urandom(20).encode('base64').strip().strip('=')
-            fp.write(_secret)
-    return _secret
+syncclient_filename = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'syncclient.js')
 
 
 class UserStorage(object):
@@ -35,21 +22,20 @@ class UserStorage(object):
         self.dir = dir
         self.timer = timer
 
-    def for_user(self, username, domain):
-        dir = os.path.join(self.dir, urllib.quote(domain, ''))
+    def for_user(self, domain, username, bucket):
+        dir = os.path.join(self.dir, urllib.quote(domain, ''), urllib.quote(username, ''), urllib.quote(bucket, ''))
         if not os.path.exists(dir):
             os.makedirs(dir)
-        filename = os.path.join(dir, urllib.quote(username, ''))
-        col_filename = filename + '.collection_id'
+        col_filename = os.path.join(dir, 'collection_id.txt')
         if not os.path.exists(col_filename):
-            collection_id = str(int(self.timer()) % 100000)
+            collection_id = str(int(self.timer() * 100) % 1000000)
             with open(col_filename, 'wb') as fp:
                 fp.write(collection_id)
         else:
             with open(col_filename, 'rb') as fp:
                 collection_id = fp.read()
-        filename = filename + '.db'
-        db = Database(filename)
+        db_name = os.path.join(dir, 'database')
+        db = Database(db_name)
         db.collection_id = collection_id
         return db
 
@@ -60,18 +46,24 @@ class UserStorage(object):
 
 class Application(object):
 
-    def __init__(self, storage, mock_browserid=False,
-                 remove_browserid=False):
+    def __init__(self, storage=None, dir=None, mock_browserid=False,
+                 remove_browserid=False,
+                 include_syncclient=False):
+        if storage is None and dir:
+            storage = UserStorage(dir)
         self.storage = storage
         self.mock_browserid = mock_browserid
         self.remove_browserid = remove_browserid
+        self.include_syncclient = include_syncclient
+        self._syncclient_app = None
+        self._syncclient_mtime = None
+        self._syncclient_app_url = None
 
     def unauthorized(self, reason):
-        resp = exc.HTTPUnauthorized()
-        ## FIXME: arg, doesn't work, dumb webob.exc
-        resp.content_type = 'text/plain'
-        resp.body = reason
-        return resp
+        return Response(
+            status=401,
+            content_type='text/plain',
+            body=reason)
 
     def update_json(self, data, **kw):
         if isinstance(data, str):
@@ -79,61 +71,27 @@ class Application(object):
         data.update(kw)
         return data
 
-    def check_auth(self, auth, domain, username):
-        try:
-            type, data = auth.split(None, 1)
-        except ValueError:
-            raise exc.HTTPBadRequest("Bad Authorization header")
-        type = type.lower()
-        if type not in ('browserid', 'synctoken'):
-            raise exc.HTTPBadRequest("Bad Authorization type")
-        if type == 'browserid':
-            return self.check_browserid(data, domain, username)
-        else:
-            return self.check_synctoken(data, domain, username)
-
-    def check_browserid(self, data, domain, username):
-        if domain.startswith('https:'):
-            audience = 'https://%s' % domain
-        else:
-            audience = 'http://%s' % domain
-        if not self.remove_browserid:
-            resp = urllib.urlopen(
-                'https://browserid.org/verify', 'assertion=%s&audience=http://%s' % (
-                    urllib.quote(data), urllib.quote(audience)))
-            resp = json.loads(resp.read())
-            if not self.mock_browserid:
-                if resp['status'] != 'okay':
-                    raise self.unauthorized("Invalid assertion")
-                if resp['email'] != username:
-                    raise self.unauthorized("Invalid user in assertion")
-        return {"X-Set-Authorization": "SyncToken %s" % self.sign_auth(domain, username)}
-
-    def check_synctoken(self, data, domain, username):
-        nonce, rest = data.split(':', 1)
-        expected = self.sign_auth(domain, username, nonce)
-        if expected != data:
-            raise self.unauthorized("Invalid SyncToken")
-        return None
-
-    def sign_auth(self, domain, username, nonce=None):
-        if not nonce:
-            nonce = os.urandom(10).encode('base64').strip().strip('=')[:10]
-        result = hmac.new(nonce, '%s %s' % (domain, username), hashlib.sha1).digest()
-        result = result.encode('base64').strip().strip('=')
-        return '%s:%s' % (nonce, result)
-
     @wsgify
     def __call__(self, req):
+        if self.include_syncclient and req.path_info == '/syncclient.js':
+            return self.syncclient(req)
         domain = req.path_info_pop()
         username = req.path_info_pop()
+        bucket = req.path_info
+        remote_user = req.environ.get('REMOTE_USER')
+        if not remote_user:
+            return self.unauthorized('No authentication provided')
+        if '/' not in remote_user:
+            raise Exception(
+                'REMOTE_USER is not formatted as username/domain')
+        remote_username, remote_domain = remote_user.split('/', 1)
+        if remote_username != username:
+            return self.unauthorized('Incorrect authentication provided (%r)' % remote_username)
+        if remote_domain != domain:
+            return self.unauthorized('Incorrect authentication provided: bad domain (%r)' % remote_domain)
         if 'include' in req.GET and 'exclude' in req.GET:
             raise exc.HTTPBadRequest('You may only include one of "exclude" or "include"')
-        auth = req.headers.get('Authorization')
-        if not auth:
-            raise self.unauthorized('No Authorization header provided')
-        auth_headers = self.check_auth(auth, domain, username)
-        db = self.storage.for_user(username, domain)
+        db = self.storage.for_user(domain, username, bucket)
         collection_id = req.GET.get('collection_id')
         if collection_id is not None and collection_id != db.collection_id:
             req.GET.since = '0'
@@ -150,12 +108,13 @@ class Application(object):
         if not isinstance(resp_data, str):
             resp_data = json.dumps(resp_data)
         resp = Response(resp_data, content_type='application/json')
-        if auth_headers:
-            resp.headers.update(auth_headers)
         return resp
 
     def post(self, req, db):
-        data = json.loads(req.body)
+        try:
+            data = req.json
+        except ValueError:
+            raise exc.HTTPBadRequest('POST must have valid JSON body')
         data_encoded = [json.dumps(i) for i in data]
         since = int(req.GET.get('since', 0))
         counter = None
@@ -194,8 +153,14 @@ class Application(object):
         return dict(object_counters=counters)
 
     def get(self, req, db):
-        since = int(req.GET.get('since', 0))
-        limit = int(req.GET.get('limit', 0))
+        try:
+            since = int(req.GET.get('since', 0))
+        except ValueError:
+            raise exc.HTTPBadRequest('Bad value since=%s' % req.GET['since'])
+        try:
+            limit = int(req.GET.get('limit', 0))
+        except ValueError:
+            raise exc.HTTPBadRequest('Bad value limit=%s' % req.GET['limit'])
         items = db.read(since)
         if limit:
             new_items = []
@@ -222,39 +187,24 @@ class Application(object):
             objects.append((count, item))
         return dict(objects=objects)
 
-if __name__ == '__main__':
-    import optparse
-    parser = optparse.OptionParser(
-        usage='%prog [OPTIONS] DIR')
-    parser.add_option(
-        '--port', '-p',
-        metavar='PORT',
-        help="Port to serve on (default %default)",
-        default=5000,
-        type='int')
-    parser.add_option(
-        '--mock-browserid',
-        action='store_true',
-        help="Don't check browserid assertions")
-    parser.add_option(
-        '--remove-browserid',
-        action='store_true',
-        help="Don't even send browserid assertions")
-    parser.add_option(
-        '--clear',
-        action='store_true',
-        help="Clear out all existing databases")
-    options, args = parser.parse_args()
-    if not args:
-        parser.error('You give a DIR')
-    from paste.httpserver import serve
-    storage = UserStorage(args[0])
-    if options.clear:
-        print 'Clearing %s' % storage.dir
-        storage.clear()
-    app = Application(storage, mock_browserid=options.mock_browserid,
-                      remove_browserid=options.remove_browserid)
-    try:
-        serve(app, port=options.port)
-    except KeyboardInterrupt:
-        pass
+    def syncclient(self, req):
+        if self._syncclient_app:
+            mtime = os.path.getmtime(syncclient_filename)
+            if mtime > self._syncclient_mtime:
+                self._syncclient_app = None
+            if req.application_url != self._syncclient_app_url:
+                ## cache different app urls?
+                self._syncclient_app = None
+        if not self._syncclient_app:
+            self._syncclient_mtime = os.path.getmtime(syncclient_filename)
+            self._syncclient_app_url = req.application_url
+            with open(syncclient_filename) as fp:
+                content = fp.read()
+            content = content.replace(
+                'Sync.baseUrl = null',
+                'Sync.baseUrl = %r' % req.application_url)
+            self._syncclient_app = Response(
+                content_type='text/javascript',
+                conditional_response=True,
+                body=content)
+        return self._syncclient_app
