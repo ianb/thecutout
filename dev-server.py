61b74d54b55bd74e9f0448afd30c03b78b0f0036
Ian Bicking <ianb@colorstudy.com>
2012-09-06 16:31:54 -0500

    Added a dev-server script for running examples and other testing

---
+++ dev-server.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import optparse
+import sys
+import os
+import shutil
+import site
+
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, here)
+
+path = here
+checks = ['vendor', 'vendor-binary']
+for i in range(3):
+    for check in checks:
+        dir = os.path.join(path, check)
+        if os.path.exists(dir):
+            print 'Adding', dir
+            site.addsitedir(dir)
+    path = os.path.dirname(path)
+
+parser = optparse.OptionParser(
+    usage='%prog [OPTIONS] [[path=]STATIC_DIR]',
+    description="""\
+Serves up logdb at http://localhost:8088/sync/syncclient.js
+
+Also serves up static files.  You may provide one or many STATIC_DIR
+entries.  You may give prefixes for these (e.g.,
+/test1=~/src/myproject/htdocs)
+""")
+
+parser.add_option('-H', '--host', metavar='HOST', default='localhost',
+                  help='Host (interface) to serve on; 0.0.0.0 means serve publicly')
+parser.add_option('-p', '--port', metavar='PORT', default='8088',
+                  help='Port to serve on')
+parser.add_option('--dir', metavar='DIRECTORY', default='./data',
+                  help='Directory to store files in')
+parser.add_option('--clear', action='store_true',
+                  help='Clear DIRECTORY on startup')
+
+from paste.urlmap import URLMap
+from paste.httpserver import serve
+from webob.static import DirectoryApp
+
+
+def main():
+    options, args = parser.parse_args()
+    if options.clear:
+        if os.path.exists(options.dir):
+            print 'Deleting %s' % options.dir
+            shutil.rmtree(options.dir)
+    mapper = URLMap()
+    for arg in args:
+        if '=' in arg:
+            path, dir = arg.split('=', 1)
+        else:
+            path, dir = '/', arg
+        mapper[path] = DirectoryApp(dir)
+    from logdb.sync import Application
+    db_app = Application(dir=options.dir, include_syncclient=True)
+    mapper['/sync'] = db_app
+    serve(mapper, host=options.host, port=int(options.port))
+
+
+if __name__ == '__main__':
+    main()
