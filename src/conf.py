a600e3c4baf19ddc92276ee852a9aa47de974868
Ian Bicking <ianb@colorstudy.com>
2012-10-26 17:14:17 -0500

    Rearrange to put the root of the site in the root of the repo

--- src/conf.py
+++ src/conf.py
@@ -64,7 +64,7 @@ release = '0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'pylons_sphinx_theme']
+exclude_patterns = ['_src', 'pylons_sphinx_theme']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 default_role = "code"
